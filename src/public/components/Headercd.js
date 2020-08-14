ca4116253e369724e266709505fa4c67d5d0d350
frank.gairal <frank@autofi.io>
2020-08-14 13:30:39 -0700

    fix(rename): rename

--- src/public/components/Headercd.js
+++
@@ -1,7 +0,0 @@
-import { h, text } from 'https://unpkg.com/hyperapp';
-
-export default ({ color, name, status }) =>
-  h('div', { class: 'flex justify-between items-center' }, [
-    h('h2', { class: 'font-bold text-xl mb-2 text-gray-200' }, text(name)),
-    h('span', { class: { 'font-bold': true, [`text-${color}-900`]: true, } }, text(status)),
-  ]);
