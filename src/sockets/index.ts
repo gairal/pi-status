d6ad100fb6e7f352ebfefadbe3118432a340f013
frank.gairal <frank@autofi.io>
2020-08-18 12:46:16 -0700

    fix9processes): removing auto stop which doesn't seem to make differences

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -4,7 +4,6 @@ import pm2 from './pm2';
 import system from './system';
 import SocketController from './SocketController';
 
-const MAX_ITERATIONS = 1000;
 const getClients = (): Promise<string[]> =>
   new Promise((resolve) => {
     io.clients((err: Error, clients: string[]) => {
@@ -18,8 +17,6 @@ const getClients = (): Promise<string[]> =>
   });
 
 class Loop {
-  private count = MAX_ITERATIONS;
-
   private isRunning = false;
 
   constructor(
@@ -35,17 +32,12 @@ class Loop {
     return Promise.all(this.sockets.map((s) => s.emit()));
   }
 
-  private static async hasClient(): Promise<boolean> {
+  public static async hasClient(): Promise<boolean> {
     return getClients().then((clients) => !!clients.length);
   }
 
   private async loop(): Promise<void> {
-    while (this.isRunning && this.count > 0) {
-      logger.debug(`loop: ${this.count}`);
-
-      // avoid infinite loop
-      this.count -= 1;
-
+    while (this.isRunning) {
       try {
         // eslint-disable-next-line no-await-in-loop
         await this.emit();
@@ -57,67 +49,30 @@ class Loop {
     }
   }
 
-  private async onDisconnect() {
-    logger.debug('disconnect');
-
-    if (!(await Loop.hasClient())) {
-      this.stop();
-    }
-  }
-
-  /**
-   * Rest the loop counter
-   */
-  private reset(): void {
-    logger.debug('reset');
-    this.count = MAX_ITERATIONS;
-  }
-
   /**
    * sets the running flag to true
    * resets the loop counter
    * starts the update loop
    */
-  private start(): void {
+  public start(): void {
     logger.debug('start');
     this.isRunning = true;
-    this.reset();
     this.loop();
   }
 
   /**
    * stops the update loop
    */
-  private async stop(): Promise<void> {
+  public async stop(): Promise<void> {
     logger.debug('stop');
     this.isRunning = false;
     // ensures we don't start until we actually got out of the loop
     await this.delay();
   }
 
-  private async restart(): Promise<void> {
-    logger.debug('restart');
-    await this.stop();
-    this.start();
-  }
-
-  private init() {
-    io.on('connection', async (socket) => {
-      if (this.isRunning) {
-        // for each new Connection we're simply resetting the loops
-        this.reset();
-      } else {
-        //  if the loop was stopped then we restart it
-        await this.restart();
-      }
-
-      socket.on('disconnect', this.onDisconnect.bind(this));
-    });
-  }
-
   static create(sockets: SocketController<unknown>[], interval?: number): Loop {
     const loop = new this(sockets, interval);
-    loop.init();
+    loop.start();
     return loop;
   }
 }
