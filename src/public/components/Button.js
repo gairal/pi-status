bcf6e085959c456a7aa5b0a22be85690ffbce037
frank.gairal <frank@autofi.io>
2020-08-17 22:13:15 -0700

    fix(style): fix UI

--- src/public/components/Button.js
+++ src/public/components/Button.js
@@ -1,11 +1,12 @@
 import { h, text } from 'https://unpkg.com/hyperapp';
 
-export default (str, { color = 'blue', onclick }) =>
+export default (str, { color = 'blue', onclick, title = '' }) =>
   h(
     'button',
     {
       class: `bg-${color}-500 hover:bg-${color}-700 text-white font-bold py-1 px-2 rounded`,
       onclick,
+      title,
     },
     text(str)
   );
--- src/public/components/Header.js
+++ src/public/components/Header.js
@@ -2,17 +2,18 @@ import { h } from 'https://unpkg.com/hyperapp';
 import H2 from './H2.js';
 import Button from './Button.js';
 
-const act = (name, action) => () =>
+const act = (name, action) => (state) => {
   fetch(`/processes/${name}/${action}`, { method: 'POST' });
+  return state;
+};
 
 export default ({ name, status }) => {
-  const action = status === 'online' ? 'stop' : 'restart';
+  const isOnline = status === 'online';
+  const action = isOnline ? 'stop' : 'restart';
+  const color = isOnline ? 'red' : 'green';
 
   return h('div', { class: 'flex justify-between items-start' }, [
     H2(name),
-    Button(status, {
-      color: status === 'online' ? 'red' : 'green',
-      onclick: act(name, action),
-    }),
+    Button(status, { color, onclick: act(name, action), title: action }),
   ]);
 };
